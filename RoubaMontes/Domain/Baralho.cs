1f54aac0f4d27b4d68512f00d626dd3f63ec6899
Sameu Araujo <oliveira.samuel.edu@gmail.com>
2024-11-30 20:16:24 -0300

    Criada classe baralho / Atualizado UML da classe

--- RoubaMontes/Domain/Baralho.cs
+++ RoubaMontes/Domain/Baralho.cs
@@ -8,15 +8,20 @@ namespace RoubaMontes.Domain
 {
     public class Baralho
     {
-        public Dictionary<int, char> Naipes = new Dictionary<int, char>
+        private Dictionary<int, char> Naipes = new Dictionary<int, char>
         {
             {1, '♥'}, {2,'♦' },{3,'♠'},{4,'♣'}
         };
         public int TotalDeCartas { get; private set; }
 
-        public Queue<Carta> Cartas { get; private set; }
+        public Carta[] Cartas { get; private set; }
 
         private int _quantidadeDeCartasPorJogador = 13;
+
+        private Random _cartaEscolhida;
+
+        private int _ultimaCarta;
+
 
         /// <summary>
         /// Cria uma instância da classe baralho contendo a quantidade de cartas do baralho relativa a cada jogador
@@ -27,46 +32,99 @@ namespace RoubaMontes.Domain
         /// <return>Retorna uma instância de Baralho com no mínimo 2 jogadores</return>
         public Baralho(int totalDeJogadores)
         {
-            Cartas = new Queue<Carta>();
-
+            _cartaEscolhida = new Random();
             int totalDeCartas;
             if (totalDeJogadores < 2)
             {
                 totalDeJogadores = 2;
             }
-            totalDeCartas = totalDeJogadores * 13;
+            totalDeCartas = totalDeJogadores * _quantidadeDeCartasPorJogador;
 
             if (totalDeCartas < 52) totalDeCartas = 52;
 
+            Cartas = new Carta[totalDeCartas];
+            _ultimaCarta = Cartas.Length - 1;
+
             InstancirBaralho(totalDeCartas);
+            Embaralhar();
         }
 
-        public void DistribuirCarta(Jogador jogadorDaVez)
+        public void RetirarCarta(Jogador jogadorDaVez)
         {
+            if (_ultimaCarta == 0) throw new ArgumentOutOfRangeException("Não há mais cartas para serem compradas no baralho.");
 
+            jogadorDaVez.ComprarCarta(Cartas[_ultimaCarta]);
+            _ultimaCarta--;
+        }
+
+        private void InstancirBaralho(int totalDeCartas)
+        {
+            int indexDoNaipe = 1;
+            int numeroDaCarta = 1;
+
+            for (int i = 0; i < totalDeCartas; i++)
+            {
+                if (Naipes.TryGetValue(indexDoNaipe, out char charDoNaipe)) // usa o indexDoNaipe para achar o naipe
+                {
+                    Cartas[i] = new Carta(numeroDaCarta, charDoNaipe);
+                    indexDoNaipe++;
+                    numeroDaCarta++;
+                    if (indexDoNaipe > 4) indexDoNaipe = 1;
+                    if (numeroDaCarta > 13) numeroDaCarta = 1;
+                }
+            }
+        }
+
+        private void Embaralhar()
+        {
+            int ultima = Cartas.Length - 1;
+            Fisher_YatesShuffle(ultima);
         }
 
         /// <summary>
-        /// Preenche a Queue de cartas aleatóriamente
+        /// Ordena de forma aleatória
+        /// Usa o último índice lógico e troca com um aleatório, então decrementa o último.
+        /// Como para na metade não garante total aleatóriedade, mas como o índice trocado com último é aleatório também garante que todo jogo seja diferente.
         /// </summary>
-        /// <param name="totalDeCartas">Total de cartas do baralho</param>
-        private void InstancirBaralho(int totalDeCartas)
+        /// <param name="ultima">O índice a ser trocado - Iniciamente Length - 1</param>
+        private void Fisher_YatesShuffle(int ultima)
         {
-            int indexNaipe = 1;
-            int numCarta = 1;
-            for (int i = 0; i < totalDeCartas; i++)
-            {
-                if(Naipes.TryGetValue(indexNaipe, out char charNaipe))
-                {
-                    Cartas.Enqueue(new Carta(indexNaipe, charNaipe));
-                }
-                indexNaipe++;
-                numCarta++;
+            if (ultima == Cartas.Length / 2) return;
 
-                if (indexNaipe == 5) indexNaipe = 1;
-                if (numCarta == 14) numCarta = 1;
-            }
+            int carta = _cartaEscolhida.Next(0, ultima);
+
+            TrocarPosicao(carta, ultima);
+
+            Fisher_YatesShuffle(ultima - 1);
         }
+
+        private void TrocarPosicao(int indice1, int indice2)
+        {
+            Carta temp = Cartas[indice1];
+            Cartas[indice1] = Cartas[indice2];
+            Cartas[2] = temp;
+        }
+
+        public override string ToString()
+        {
+            return ParseBaralhoParaString();
+        }
+
+        private string ParseBaralhoParaString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int contador = 0;
+            foreach (var carta in Cartas)
+            {
+                if (contador % 13 == 0 && contador != 0) sb.Append("\n");
+                sb.Append($"{carta}, ");
+                contador++;
+            }
+
+            return sb.ToString();
+        }
+
 
     }
 }
--- RoubaMontes/Program.cs
+++ RoubaMontes/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using RoubaMontes.Domain;
+
+Console.WriteLine("Mostrando Baralho: ");
+Baralho baralho = new Baralho(4);
+string string0 = baralho.ToString();
+Console.WriteLine(string0);
